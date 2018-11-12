98157a53a4625b02ac173b6e4baa27ef15088a30
Kevin <kjh7796@gmail.com>
2018-11-11 21:41:38 -0500

    home page style changes

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from home import views
+from backend import views as bViews
 
 urlpatterns = [
     path('', views.homePage, name="home"),
+    path('api', bViews.CopyDataView, name="root")
 ]
